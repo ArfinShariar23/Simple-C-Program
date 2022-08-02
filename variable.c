d0a935c06b937ff326cc44b354ce632cfda00c0b
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-08-03 00:14:49 +0600

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,13 @@
+//Variable in C 
+#include<stdio.h>
+int main()
+{
+    int age = 10;  // age is a variable where value is age
+    char ch = 'a'; // ch is a variable where value is 'a'
+    double mark = 10.56; //mark is a variable where value is 10.56
+    
+    printf("Your Age is = %d\n",age);
+    printf("Your Favourite Character = %c\n",ch);
+    printf("Your Last Exam Mark = %lf\n",mark);
+    return 0;
+}
