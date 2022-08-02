9cb99862ad429f7a1165fef76a6a3885ba78d67e
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-08-03 00:28:40 +0600

    Add files via upload

---
+++ Constant Varible.c
@@ -0,0 +1,12 @@
+//Constant Variable in C 
+
+#include<stdio.h>
+int main()
+{
+    const double pi = 3.14;
+    printf("The Value of PI = %lf\n",pi);
+    printf("Thank you");
+    
+    
+    return 0;
+}
---
+++ User_Input.c
@@ -0,0 +1,15 @@
+//Taking User Input in C 
+#include<stdio.h>
+int main(){
+    int num1;
+    int num2;
+    printf("Enter a number = \n");
+    scanf("%d",&num1);
+    printf("Enter Another Number = \n");
+    scanf("%d",&num2);
+    
+    printf("Your Entered First Number is = %d\n",num1);
+    printf("Your Entered Second Number is = %d\n",num2);
+    printf("Thank you");
+    return 0;
+}
