8e3fb3d26f4d8c43cbefeddbc2363723567ca93b
Arfin Shariar <76444319+ArfinShariar23@users.noreply.github.com>
2022-08-03 13:24:15 +0600

    Add files via upload

---
+++ ASCII to Char Converter.c
@@ -0,0 +1,9 @@
+// Find the Charecter depends on ASCII value on c
+#include<stdio.h>
+int main(){
+    int number;
+    printf("Enter Any Number: ");
+    scanf("%d",&number);
+    
+    printf("The ASCII Charecter is: %c",number);
+}
---
+++ Char to ASCII Converter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+	
+	char ch;
+	printf("Enter Any Charecter: ");
+	scanf("%c",&ch);
+	
+	printf("Entered Charecter ASCII Value is:%d",ch);
+	
+	return 0;
+}
